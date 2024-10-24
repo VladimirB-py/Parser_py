495572096ac84539eea5df0bc40a8041f0f41748
Vladimir <vladimirberkutel@gmail.com>
2024-10-24 14:49:29 +0300

    init

---
+++ Parser/mainParser.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+link = "https://3.python-requests.org/user/quickstart/"
+
+res = requests.get(link).text
+soup=BeautifulSoup(res, 'lxml')
+block=soup.find('div', id='binary-response-content')
+h2_find=block.find('h2')
+#print(block)
+print(h2_find)
