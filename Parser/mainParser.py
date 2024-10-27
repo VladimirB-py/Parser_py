d2a7e1df04a9e99d86ef26daa461005426252ad0
Vladimir <vladimirberkutel@gmail.com>
2024-10-27 20:22:40 +0300

    new branch and use session

--- Parser/mainParser.py
+++ Parser/mainParser.py
@@ -2,13 +2,16 @@ import fake_useragent
 import requests
 from bs4 import BeautifulSoup
 
+link = "https://httpbin.org/"
+
 user = fake_useragent.UserAgent().random
 header = {'user-agent': user}
-link = "https://3.python-requests.org/user/quickstart/"
 
-res = requests.get(link, headers=header).text
-soup = BeautifulSoup(res, 'lxml')
-block = soup.find('div', id='binary-response-content')
-h2_find = block.find('h2')
-# print(block)
-print(f'text - {h2_find}')
+with requests.Session() as s:
+    res=s.get(link, headers=header).text
+
+    soup = BeautifulSoup(res, 'lxml')
+    block = soup.find('span', id='//*[@id="swagger-ui"]/div/div[2]/div[3]/section/div/div[7]')
+    #span_find = block.find('span')
+    print(soup, block)
+    #print(f'text - {h2_find}')
