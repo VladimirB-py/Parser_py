4556e6a9d0f3f07d31a1b1b47979254cb8384673
Vladimir <vladimirberkutel@gmail.com>
2024-10-27 17:48:18 +0300

    import fake_useragent prepare to auth todo

--- Parser/mainParser.py
+++ Parser/mainParser.py
@@ -2,13 +2,13 @@ import fake_useragent
 import requests
 from bs4 import BeautifulSoup
 
-user=fake_useragent.UserAgent().random
-header={'user-agent': user}
+user = fake_useragent.UserAgent().random
+header = {'user-agent': user}
 link = "https://3.python-requests.org/user/quickstart/"
 
 res = requests.get(link, headers=header).text
-soup=BeautifulSoup(res, 'lxml')
-block=soup.find('div', id='binary-response-content')
-h2_find=block.find('h2')
-#print(block)
+soup = BeautifulSoup(res, 'lxml')
+block = soup.find('div', id='binary-response-content')
+h2_find = block.find('h2')
+# print(block)
 print(f'text - {h2_find}')
